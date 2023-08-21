90283e2a6d8238e66e582e7f6348b6ca93b02032
vtea <srezhou@gmail.com>
2023-08-21 23:05:31 +0800

    mjprm

--- src/app/ui/Navigation.tsx
+++ src/app/ui/Navigation.tsx
@@ -2,60 +2,60 @@
 
 import React, { useState } from "react";
 import {
-  BgColorsOutlined,
-  ExperimentOutlined,
-  ThunderboltOutlined,
-  GithubOutlined,
-  ToolOutlined,
+    BgColorsOutlined,
+    ExperimentOutlined,
+    ThunderboltOutlined,
+    GithubOutlined,
+    ToolOutlined,
 } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Menu, Row, Col } from "antd";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 const items: MenuProps["items"] = [
-  {
-    label: <Link href='/'>IMGPrompt</Link>,
-    key: "/",
-    icon: <BgColorsOutlined />,
-  },
-  {
-    label: (
-      <a
-        href='https://aiprm.cn2.ltd'
-        target='_blank'
-        rel='noopener noreferrer'>
-        Aiprompt
-      </a>
-    ),
-    key: "Aiprompt",
-    icon: <ThunderboltOutlined />,
-  },
-  {
-    label: (
-      <a
-        href='https://chat.mice.pub/'
-        target='_blank'
-        rel='noopener noreferrer'>
-        Chat Online
-      </a>
-    ),
-    key: "chatonline",
-    icon: <ExperimentOutlined />,
-  },
-  {
-    label: (
-      <a
-        href='https://doc.mice.pub'
-        target='_blank'
-        rel='noopener noreferrer'>
-        Doc
-      </a>
-    ),
-    key: "Doc",
-    icon: <ToolOutlined />,
-  },
+    {
+        label: <Link href='/'>IMGPrompt</Link>,
+        key: "/",
+        icon: <BgColorsOutlined />,
+    },
+    {
+        label: (
+            <a
+                href='https://aiprm.cn2.ltd'
+                target='_blank'
+                rel='noopener noreferrer'>
+                Aiprompt
+            </a>
+        ),
+        key: "LearnData",
+        icon: <ThunderboltOutlined />,
+    },
+    {
+        label: (
+            <a
+                href='https://chat.mice.pub/'
+                target='_blank'
+                rel='noopener noreferrer'>
+                Chat Onlie
+            </a>
+        ),
+        key: "aishort",
+        icon: <ExperimentOutlined />,
+    },
+    {
+        label: (
+            <a
+                href='https://doc.mice.pub'
+                target='_blank'
+                rel='noopener noreferrer'>
+                Doc
+            </a>
+        ),
+        key: "Tools",
+        icon: <ToolOutlined />,
+    },
 ];
-
 export function Navigation() {
     const pathname = usePathname();
     const [current, setCurrent] = useState(pathname);
