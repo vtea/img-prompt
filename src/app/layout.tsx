3763256130c67d65b3bdfd8cfabe708bd308be5a
vtea <srezhou@gmail.com>
2023-08-21 22:37:06 +0800

    mjprompt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,28 +20,6 @@ export default function RootLayout({
       <body>
         <Navigation />
         {children}
-        <Script
-          id='piwik'
-          dangerouslySetInnerHTML={{
-            __html: `
-            var _paq = window._paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="https://piwik.seoipo.com/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '10']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-          `,
-          }}
-        />
-        <Script
-          src='https://oss.newzone.top/instantpage.min.js'
-          type='module'
-          strategy='lazyOnload'
-        />
       </body>
     </html>
   );
